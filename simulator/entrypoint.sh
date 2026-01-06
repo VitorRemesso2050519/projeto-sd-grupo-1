6c6852f57dced9fe00bb593e31e491620e808feb
Marco Ascensao <marco.ascensao@programacao.org>
2026-01-06 01:53:31 +0000

    Simplify GPX file copying in entrypoint script to include only new files

--- simulator/entrypoint.sh
+++ simulator/entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-
-# Copia apenas ficheiros que ainda não existem no destino
-for src in /tmp/gpx-files/*; do
-	dest="/app/gpx-files/$(basename "$src")"
-	if [ ! -e "$dest" ]; then
-		cp "$src" "$dest"
-	fi
-done
-echo "GPX files copied to volume (only new files)."
+cp -rn /tmp/gpx-files/* /app/gpx-files/
+echo "GPX files copied to volume."
 exec python simulator.py
